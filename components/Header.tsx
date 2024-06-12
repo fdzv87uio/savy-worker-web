fc734fd98b26b7be2e554a2b1c20c3ecf1c437c0
Roger Chung <84298258+rch-goldsnaker@users.noreply.github.com>
2024-06-12 10:45:01 -0300

    adding header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { Button } from './ui/button'
+
+import { Inter } from "next/font/google";
+import Image from 'next/image';
+const inter = Inter({ subsets: ["latin"] });
 
 const Header = () => {
   return (
-    <div className="sticky top-0 flex size-full flex-col bg-black-5 z-50">
-      <section className="glassmorphism-black flex h-[112px] w-full items-center justify-between px-4 max-md:justify-center max-md:gap-5 md:px-12">
-        <div className="flex items-center gap-4 max-md:hidden">
-          <div className="flex w-[160px] flex-col">
-            <h2 className="text-14 truncate font-semibold text-white-1">
-              Header
-            </h2>
+    <div className="sticky top-0 flex size-full z-50 w-full bg-black-1 border-spacing-0">
+      <section className="flex h-[83px] w-full items-center justify-between px-4">
+        <div className="flex items-center w-full">
+          <div className="flex justify-between w-full px-16">
+            <div className='flex justify-center items-center gap-3'>
+              <h1 className="text-4xl font-normal text-gray-1 uppercase">
+                Curcleup
+              </h1>
+              <div className='w-[7px] h-[7px] bg-primary-1 rounded-full'></div>
+            </div>
+            <li className='flex gap-7'>
+              <ul className='text-2xl font-normal text-green-1 uppercase'>Home</ul>
+              <ul className='text-2xl font-normal text-white-1 uppercase'>Events</ul>
+              <ul className='text-2xl font-normal text-white-1 uppercase'>Tournaments</ul>
+            </li>
+            <div className='flex gap-5'>
+              <Button variant="primary" className={`uppercase text-sm ${inter.className} gap-3`}>
+                <Image src="/icons/signUp.svg" alt='icon' width={20} height={20} />
+                Sign Up
+              </Button>
+              <Button variant="secondary" className={`uppercase text-sm ${inter.className} gap-3`}>
+                Sign In
+                <Image src="/icons/signIn.svg" alt='icon' width={20} height={20} />
+              </Button>
+            </div>
           </div>
         </div>
       </section>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,10 +14,10 @@ const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondary:"bg-secondary-1 text-white-2 hover:bg-secondary-1/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        primary:"bg-primary-1 text-white-1 hover:bg-primary-1/90",
       },
       size: {
         default: "h-10 px-4 py-2",
