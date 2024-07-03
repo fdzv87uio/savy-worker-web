85c73beef876bf4fb3757c29109a68f46e042525
Fausto Zambrano <fausto@faqx.com>
2024-07-03 16:30:46 -0500

    adding find by user email util to homepage frontend

--- components/UserBanner.tsx
+++ components/UserBanner.tsx
@@ -4,7 +4,13 @@ import { Inter } from "next/font/google";
 import Link from 'next/link';
 const inter = Inter({ subsets: ["latin"] });
 
-const UserBanner = () => {
+interface UserBannerProps {
+  name: string;
+  lastname: string;
+  email: string;
+}
+
+function UserBanner({ name, lastname, email }: UserBannerProps) {
   return (
     <div className='flex w-full relative mt-10'>
       <div className='flex items-center w-full rounded-full bg-gradient-to-r from-[#3772AD] to-[#3772AD80] md:via-[#3772AD] md:to-[#3772AD00] h-[116px]'>
@@ -17,9 +23,9 @@ const UserBanner = () => {
             <Image src="/images/userExample.png" alt="Profile photo" className="rounded-full" width={63} height={63} />
           </Link>
           <div className='flex flex-col justify-start items-start gap-1'>
-            <h1 className="text-xl md:text-2xl text-white-1">Hello, Andy</h1>
+            <h1 className="text-xl md:text-2xl text-white-1">Hello, {name} {lastname}</h1>
             <div className=' bg-primary-1 px-3 rounded-full'>
-              <p className={`${inter.className} text-sm text-white-1 `}>Sports Enthusiast</p>
+              <p className={`${inter.className} text-sm text-white-1 `}>{email}</p>
             </div>
             <p className={`${inter.className} text-white-1 text-sm md:text-base`}>Welcome back to our platform</p>
           </div>
--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -31,3 +31,28 @@ export async function standardLogin(email: string, password: string) {
     }
 };
 
+export async function findUserByEmail(email: string, token: string) {
+    try {
+        const apiUrl = process.env.NEXT_PUBLIC_CURCLE_API_URL + "/user/find/" + email;
+
+
+        const response = await axios.get(apiUrl, {
+            headers: {
+                'Content-Type': 'application/json', // Ajusta el tipo de contenido según tus necesidades
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+
+        const res = {
+            status: 'success',
+            data: response.data,
+        }
+        return res;
+    } catch (error: any) {
+        const res = {
+            status: 'error',
+            error: error,
+        }
+        return res;
+    }
+};
