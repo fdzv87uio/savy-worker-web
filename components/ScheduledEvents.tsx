e10b82a5580c1032d40c303d5dc4fbe75e97f1f0
Fausto Zambrano <fausto@faqx.com>
2024-07-30 15:12:32 -0500

    adding new date interval form and finishing home search bar

--- components/ScheduledEvents.tsx
+++ components/ScheduledEvents.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Inter } from "next/font/google";
 import { getAllEvents } from '@/utils/eventsUtils';
 import { getAllPreferences, getPreferenceName } from '@/utils/preferencesUtils';
+import ScheduledEventCard from './ui/scheduledEventCard';
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -37,32 +38,7 @@ function ScheduledEvents({ userInfo }: ScheduledEventsProps) {
       <h2 className='text-white-1 text-3xl md:text-4xl text-center'>Your scheduled events</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-center gap-4 mt-20">
         {events.length > 0 && events.map((event: any) => (
-          <div key={event._id} className="bg-white rounded-lg shadow-lg w-[350px] md:w-[371px] h-[457px] relative border border-[rgba(255,255,255,0.2)] bg-[#ffffff]/10">
-            <div className="flex items-center bg-[url('/images/card-bg.png')] bg-cover bg-left-bottom opacity-10 w-[350px] md:w-[371px] h-[457px]">
-            </div>
-            <div className='absolute top-0 left-0'>
-              <Image src={event.images[0]} alt={event.title} width={371} height={236} className="h-[236px] object-cover rounded-lg" />
-              <div className="p-4 flex flex-col mt-5">
-                <h3 className="text-white-1 text-xl md:text-2xl">{event.title}</h3>
-                <p className={`text-white-1 text-base md:text-lg ${inter.className}`}>Date: {event.date}</p>
-                <div className='flex items-center gap-2'>
-                  <Image src="/icons/star2.svg" alt="start" width={18} height={18} />
-                  <p className={`text-white-1 text-base md:text-lg ${inter.className}`}>{event.guestList.length} Guests</p>
-                </div>
-                <div className='flex gap-[10px]'>
-                  {event.preferenceListIds.map((y: any, key: number) => {
-                    return (
-                      <div key={`pref_${key}`} className='bg-primary-1 px-3 rounded-full'>
-                        <p className={`text-white-1 text-base md:text-lg  ${inter.className}`}>
-                          {getPreferenceName(allPrefs, y)}
-                        </p>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
+          <ScheduledEventCard key={event._id} event={event} allPrefs={allPrefs} />
         ))}
       </div>
       {events.length === 0 && (
