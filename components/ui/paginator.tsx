7f525997399ff462ab215c7840a32f77f212a5ec
Fausto Zambrano <fausto@faqx.com>
2024-08-05 17:03:43 -0500

    correction in pagination

--- components/ui/paginator.tsx
+++ components/ui/paginator.tsx
@@ -9,33 +9,35 @@ export default function Paginator({ totalPages, currentPage, setCurrentPage, scr
     return (
         <div className="w-full h-[70px] mt-[60px] mx-[5px] my-[5px] flex flex-col justify-center items-center">
             <div className="w-auto h-[70px] flex flex-row justify-between">
-                <div className="w-[50px] h-[50px] flex flex-col justify-center items-center">
-                    <div onClick={() => {
-                        if (currentPage !== 1) {
-                            handlePageChange(currentPage - 1)
-                        }
-                    }}
-                        className="cursor-pointer flex flex-col justify-center items-center">
+                <div onClick={(e: any) => {
+                    e.stopPropagation();
+                    if (currentPage !== 1) {
+                        handlePageChange(currentPage - 1)
+                    }
+                }} className="w-[50px] h-[50px] flex flex-col justify-center items-center">
+                    <div className="cursor-pointer flex flex-col justify-center items-center">
                         <img src="/icons/chevron-left.svg" width={17} height={17} />
                     </div>
                 </div>
                 {array.map((x: any, key: number) => {
                     return (
-                        <div key={`button_${key}`} className={`${currentPage === x ? 'bg-primary-1/50' : ''} w-[50px] h-[50px] flex flex-col justify-center items-center cursor-pointer rounded-full`}>
-                            <div onClick={() => { handlePageChange(x) }}
-                                className="flex flex-col justify-center items-center hover:bg-white-400/50">
+                        <div onClick={(e: any) => {
+                            e.stopPropagation();
+                            handlePageChange(x);
+                        }} key={`button_${key}`} className={`${currentPage === x ? 'bg-primary-1/50' : ''} w-[50px] h-[50px] flex flex-col justify-center items-center cursor-pointer rounded-full`}>
+                            <div className="flex flex-col justify-center items-center hover:bg-white-400/50">
                                 <p className="text-[16px] text-[#ffffff] font-bolder">{x}</p>
                             </div>
                         </div>
                     )
                 })}
-                <div className="w-[50px] h-[50px] flex flex-col justify-center items-center">
-                    <div onClick={() => {
-                        if (currentPage !== totalPages) {
-                            handlePageChange(currentPage + 1)
-                        }
-                    }}
-                        className="cursor-pointer flex flex-col justify-center items-center">
+                <div onClick={(e: any) => {
+                    e.stopPropagation();
+                    if (currentPage !== totalPages) {
+                        handlePageChange(currentPage + 1)
+                    }
+                }} className="w-[50px] h-[50px] flex flex-col justify-center items-center">
+                    <div className="cursor-pointer flex flex-col justify-center items-center">
                         <img src="/icons/chevron-right.svg" width={17} height={17} />
                     </div>
                 </div>
